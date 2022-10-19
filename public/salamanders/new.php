2544cc8ff9ce3b64eb12ea47ca72bdefd4f3c667
spmackey <spmackey@ncsu.edu>
2022-10-19 12:02:36 -0400

    Created form for new.php

--- public/salamanders/new.php
+++ public/salamanders/new.php
@@ -11,4 +11,29 @@ if($test == '404') {
 } elseif($test == 'redirect') {
   redirect_to(url_for('/salamanders/index.php'));
 }
+
+$page_title = 'Create Salamander';
+require_once(SHARED_PATH .'/salamander-header.php');
 ?>
+
+
+
+
+<div id="content">
+
+  <p><a href=<?=url_for('salamanders/index.php') ?>>&laquo; Back to Salamander List</a></p>
+  <div class="subject new">
+    <h1>Create Salamander</h1>
+
+    <form action="<?php echo url_for('/salamanders/create.php'); ?>" method="post">
+      <label for="salamanderName">Name</label>
+      <br>
+      <input type="text" name="salamanderName" id="salamanderName">
+      <br>
+      <input type="submit" value="Create Salamander">
+    </form>
+
+  </div>
+
+</div>
+<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
