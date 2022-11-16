50b6e0a943b5dc703943988b695bfe71ce0c586c
spmackey <spmackey@ncsu.edu>
2022-11-16 10:29:09 -0500

    Completed Code to display a single record

--- public/salamanders/edit.php
+++ public/salamanders/edit.php
@@ -25,15 +25,23 @@ require_once(SHARED_PATH .'/salamander-header.php');
 
   <p><a href=<?=url_for('salamanders/index.php') ?>>&laquo; Back to Salamander List</a></p>
   <div class="subject edit">
-    <h1>Stub for Edit Salamander</h1>
+    <h1>Edit Salamander</h1>
 
-    <!-- <form action="<?= url_for('/salamanders/edit.php?id=' . h(u($id))); ?>" method="post">
-      <label for="salamanderName">Name</label>
+    <form action="<?php echo url_for('salamanders/create.php'); ?>" method="post">
+      <label for="salamanderName">Name:</label>
       <br>
-      <input type="text" name="salamanderName" id="salamanderName" value="<?= h($name);?>">
+      <input type="text" name="salamanderName" id="salamanderName">
+      <br><br>
+      <label for="habitat">Habitat:</label>
       <br>
-      <input type="submit" value="Edit Salamander">
-    </form> -->
+      <textarea rows="4" cols="50" name="habitat" id="habitat"></textarea>
+      <br><br>
+      <label for="description">Description:</label>
+      <br>
+      <textarea rows="4" cols="50" name="description" id="description"></textarea>
+      <br><br>
+      <input type="submit" value="Create Salamander">
+    </form>
 
   </div>
 
--- public/salamanders/show.php
+++ public/salamanders/show.php
@@ -1,16 +1,16 @@
 <?php require_once('../../private/initialize.php'); 
 
 $id = $_GET['id'] ?? 1;
-$subject = find_salamander_by_id($id);
+$salamander = find_salamander_by_id($id);
 $page_title = 'Salamander Details';
 include(SHARED_PATH . '/salamander-header.php');
 ?>
 
 <h2>Salamander Details</h2>
 
-<p><strong>ID: </strong><?= h($subject['id']) ?> </p>
-<p><strong>Name: </strong><?= h($subject['name']) ?></p>
-<p><strong>Habitat: </strong><br><?= h($subject['habitat']) ?></p>
-<p><strong>Description: </strong><br><?= h($subject['description']) ?></p>
+<p><strong>ID: </strong><?= h($salamander['id']) ?> </p>
+<p><strong>Name: </strong><?= h($salamander['name']) ?></p>
+<p><strong>Habitat: </strong><br><?= h($salamander['habitat']) ?></p>
+<p><strong>Description: </strong><br><?= h($salamander['description']) ?></p>
 <p><a href=<?=url_for('salamanders/index.php') ?>>&laquo; Back to Salamander List</a></p>
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
